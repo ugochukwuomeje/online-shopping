36c6266827dfe580355da650b05e50171366b597
ugochukwu <ugochukwuomeje1@gmail.com>
2018-08-02 10:22:13 +0200

    my project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/foodvillage/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.foodvillage.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
